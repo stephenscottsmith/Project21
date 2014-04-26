a25da31addf4779eacd01f0401cdd2d6ac11508b
Steve <steve@tgiltd.com>
2014-04-26 11:59:37 -0700

    Added the black-test and counting-test files

---
+++ Testing/counting-test.js
@@ -0,0 +1,13 @@
+/*
+*  counting.js Test Suite
+*/
+
+$(function() {
+	
+	test("Place counting tests here!", function () {
+
+		equal("Place tests here!", "Place tests here!", "YOU WILL PLACE TESTS HERE FOR COUNTING!");
+
+	});
+
+});
