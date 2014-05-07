145a226f8e54d2015226351df142915fb21a702e
Rachel <rachelriv94@gmail.com>
2014-05-07 04:28:29 -0700

    most recent login

--- js/login.js
+++ js/login.js
@@ -22,37 +22,44 @@ $(document).ready(function() {
                 $("#loginModal").modal('hide');
                 $("#duplicateLoginAlert").show();
             }
+
+            if (data == "success"){
+                $("#loginModal").modal('hide');
+                $("#loginSuccessAlert").show();
+            }
         })
         return;
     });
+});
 
-
+$(document).ready(function() {
      $("#register_form").submit(function(event){
+        alert("GOT IN HERE");
         event.preventDefault();
-        var user = $("#username").val();
-        var pass = $("passwd").val();
+        var user = $("#usrname").val();
+        var pass = $("password").val();
         var test = $.post("/register", { username: test, password: pass });
         test.done(function(data) {
             alert(data);
 
             if (data == "success"){
-                $("#loginModal").modal('hide');
+                $("#registerModal").modal('hide');
                 $("#registerAlert").show();
         }
 
 
             if (data === "fail"){
-                $("#loginModal").modal('hide');
+                $("#registerModal").modal('hide');
                 $("#loginAlert").show();
             }
 
             if (data === "denied"){
-                $("#loginModal").modal('hide');
+                $("#registerModal").modal('hide');
                 $("#deniedLoginAlert").show();
             }
 
             if (data === "duplicate"){
-                $("#loginModal").modal('hide');
+                $("#registerModal").modal('hide');
                 $("#duplicateLoginAlert").show();
             }
         })
